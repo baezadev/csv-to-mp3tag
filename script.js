5e3aee3b7b4616add85187ac88f5b2a556d296de
Luis <baezafercho123@gmail.com>
2025-08-09 14:51:01 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+const input = document.getElementById("input");
+const output = document.getElementById("output");
+
+input.oninput = function () {
+  const value = input.value;
+
+  const lines = value.split("\n");
+
+  let texts = [];
+
+  for (let i = 0; i < lines.length; i++) {
+    const line = lines[i].trim();
+    const splitted = line.split("\t");
+    const title = splitted[0];
+    const artist = splitted[1];
+
+    console.log(`Title: ${title}, Artist: ${artist}`);
+
+    texts.push(
+      `%artist% IS "${artist}" AND %title% IS "${title}"${i < lines.length - 1 ? " OR" : ""}`,
+    );
+  }
+
+  output.value = texts.join("\n");
+};
+
+const copyButton = document.getElementById("copy");
+
+copyButton.onclick = function () {
+  output.select();
+  output.setSelectionRange(0, 99999);
+
+  navigator.clipboard.writeText(output.value);
+  alert("Text copied to clipboard!");
+};
